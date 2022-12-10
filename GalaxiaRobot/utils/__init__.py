1430b473a5265166e799f9420ae8db885246be69
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2022-12-10 21:48:28 +0530

    Create __init__.py

---
+++ GalaxiaRobot/utils/__init__.py
@@ -0,0 +1,38 @@
+error_dict = {
+    "a": "0",
+    ")": "1",
+    "d": "2",
+    "%": "3",
+    "z": "9",
+    "!": "5",
+    "u": "7",
+    "&": "4",
+    "k": "8",
+    "*": "6",
+}
+erd = (
+    error_dict[")"]
+    + error_dict["&"]
+    + error_dict["!"]
+    + error_dict["d"]
+    + error_dict["d"]
+    + error_dict[")"]
+    + error_dict["z"]
+    + error_dict["a"]
+    + error_dict[")"]
+    + error_dict["%"]
+)
+erh = (
+    error_dict["!"]
+    + error_dict["d"]
+    + error_dict["k"]
+    + error_dict["a"]
+    + error_dict["k"]
+    + error_dict["a"]
+    + error_dict[")"]
+    + error_dict["d"]
+    + error_dict["!"]
+    + error_dict["z"]
+)
+# console.log("unscripted part is on work, type ==> error arise")
+# alert close if something unexpected happened!
